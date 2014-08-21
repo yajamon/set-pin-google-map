ed24c2414f40b04c9d3d181c8945274396fa2d9b
yajamon <yajamon.tatsuki@gmail.com>
2014-08-21 16:49:49 +0900

    Use markers object

--- js/pages/multi_pin.js
+++ js/pages/multi_pin.js
@@ -3,16 +3,19 @@ var totyo = {
     lat: 35.689634,
     lng: 139.692101,
 };
-// JR新宿駅
-var station = {
+
+var markers = new Markers();
+
+markers.add({
+    title: 'JR新宿駅',
     lat: 35.690891,
     lng: 139.700229,
-};
-// 新宿中央公園
-var park = {
+});
+markers.add({
+    title: '新宿中央公園',
     lat: 35.689773,
     lng: 139.689626,
-};
+});
 
 google.maps.event.addDomListener(window, 'load', function() {
     var mapdiv = document.getElementById('map_canvas');
@@ -24,15 +27,6 @@ google.maps.event.addDomListener(window, 'load', function() {
     };
     var map = new google.maps.Map(mapdiv, myOptions);
 
-    var marker1 = new google.maps.Marker({
-        position: new google.maps.LatLng(station.lat, station.lng),
-        map: map,
-        title: '新宿駅'
-    });
-    var marker2 = new google.maps.Marker({
-        position: new google.maps.LatLng(park.lat, park.lng),
-        map: map,
-        title: '新宿中央公園'
-    });
+    markers.setAllMap(map);
 });
 
